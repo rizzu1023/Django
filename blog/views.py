f20dd3dd45268900e09d0e6799047d394f209e29
rizzu1023 <mrizwan9022@gmail.com>
2018-11-05 15:53:32 +0530

    post_project

---
+++ blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+# Create your views here.
+posts =[
+	{
+			'author':'CoreyMS',
+			'title':'Blog_post',
+			'content':'First post content',
+			'date_posted':'August 25, 2031'
+	},
+	{
+
+			'author':'Mohammed Rizwan',
+			'title':'Blog_post 2',
+			'content':'Second post content',
+			'date_posted':'November 4, 2018'
+	}
+]
+
+
+def home(request):
+	context={
+		'rizwan':posts
+	}
+	return render(request, 'blog/home.html' ,context)
+
+
+
+def about(request):
+	# return HttpResponse("<h1>blog About</h1>")
+	return render(request, 'blog/about.html' , {'title':'django'})
+
