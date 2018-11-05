4359e824cc8995778520c602c652116259a659a4
rizzu1023 <mrizwan9022@gmail.com>
2018-11-05 19:49:25 +0530

    database

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
+from .models import post
 # from django.http import HttpResponse
 # Create your views here.
-posts =[
-	{
-			'author':'CoreyMS',
-			'title':'Blog_post',
-			'content':'First post content',
-			'date_posted':'August 25, 2031'
-	},
-	{
+# posts =[
+# 	{
+# 			'author':'CoreyMS',
+# 			'title':'Blog_post',
+# 			'content':'First post content',
+# 			'date_posted':'August 25, 2031'
+# 	},
+# 	{
 
-			'author':'Mohammed Rizwan',
-			'title':'Blog_post 2',
-			'content':'Second post content',
-			'date_posted':'November 4, 2018'
-	}
-]
+# 			'author':'Mohammed Rizwan',
+# 			'title':'Blog_post 2',
+# 			'content':'Second post content',
+# 			'date_posted':'November 4, 2018'
+# 	}
+# ]
 
 
 def home(request):
 	context={
-		'rizwan':posts
+		'rizwan': post.objects.all()
 	}
 	return render(request, 'blog/home.html' ,context)
 
