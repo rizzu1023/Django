e86c0242f246c8e414291d42ba93b64f2e54cd50
rizzu1023 <mrizwan9022@gmail.com>
2018-11-08 14:49:14 +0530

    registration

--- users/views.py
+++ users/views.py
@@ -11,8 +11,9 @@ def register(request):
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get("username")
-			messages.success(request, f'Account created for {username}!')
-			return redirect('blog-home')
+			# messages.success(request, f'Account created for {username}!')
+			messages.success(request, f'Your Account has been created! You are able to login')
+			return redirect('login')
 	else:
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form':form})
